750582bf32db140c278f522444d971523e5290e6
nicoletadinu83 <nicoletadinu1009@gmail.com>
2021-02-02 03:29:57 +0200

    add methods: Put, Post, Get, Delete  in the class AddressController, PersonController

--- HospitalMAnagement/src/main/java/HospitalManagement/com/HospitalMAnagement/controller/PersonController.java
+++ HospitalMAnagement/src/main/java/HospitalManagement/com/HospitalMAnagement/controller/PersonController.java
@@ -7,9 +7,7 @@ import HospitalManagement.com.HospitalMAnagement.service.PersonServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +29,40 @@ public class PersonController {
         List<PacientDto> pacientDtoList = personService.getAllPacients();
         System.out.println("Pacient list are: " + pacientDtoList);
         return new ResponseEntity(pacientDtoList, HttpStatus.OK);
+    }
+
+    @GetMapping("getPacient/{id}")
+    public ResponseEntity<PacientDto> getPacientById(@PathVariable Long id) {
+        PacientDto pacientDto = personService.getPacientById(id);
+        System.out.println("I found the pacient: " + pacientDto);
+        return new ResponseEntity(pacientDto, HttpStatus.OK);
+    }
+
+    @GetMapping("getDoctor/{id}")
+    public ResponseEntity<DoctorDto> getDoctorById(@PathVariable Long id) {
+        DoctorDto doctorDto = personService.getDoctorById(id);
+        return new ResponseEntity(doctorDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("deletePerson/{id}")
+    public ResponseEntity deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+        System.out.println(" The person with id = " + id + " was deleted.");
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("addPerson")
+    public ResponseEntity addPerson(@RequestBody PersonDto personDto){
+        personService.addPerson(personDto);
+        System.out.println("Was addes the  person : "+ personDto);
+         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("updatePerson")
+    public ResponseEntity updatePerson(@RequestBody PersonDto personDto){
+        personService.updatePerson(personDto);
+        System.out.println("The person was updated");
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
